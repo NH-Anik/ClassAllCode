8ec16a96d6570b490e4a512b31928e87685abc38
nh-anik <niamulhasan515@gmail.com>
2025-03-20 17:20:09 +0600

    2nd class

---
+++ Dipto class code/dropDownWithJS/js/index.js
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", function() {
+    const dropdownToggle = document.querySelector(".dropdown-toggle");
+    const dropdownMenu = document.querySelector(".dropdown");
+
+    dropdownToggle.addEventListener("click", function(event) {
+        event.preventDefault();
+        dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
+    });
+
+    document.addEventListener("click", function(event) {
+        if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.style.display = "none";
+        }
+    });
+// });
